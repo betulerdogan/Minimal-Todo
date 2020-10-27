92fbaec11cb8407e6799afe9119f4186eb4ba25b
Betül Erdoğan <betulerdo6an@gmail.com>
2020-10-27 22:57:52 +0300

    Migrate to AndroidX

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
@@ -2,8 +2,8 @@ package com.example.avjindersinghsekhon.minimaltodo.AddToDo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultActivity;
